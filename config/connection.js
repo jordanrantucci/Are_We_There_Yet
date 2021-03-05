84558d87ae455c0a4b73a6206d1150fe1adc76ba
Jordan Rantucci <jordanrantucci@gmail.com>
2021-03-04 20:58:01 -0500

    adding MVC file structure

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+// Set up MySQL connection.
+const mysql = require('mysql');
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  // NOTE: Be sure to add your MySQL password here!
+  password: process.env.DB_PASS,
+  database: 'burger_db',
+});
+};
+
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devour BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
