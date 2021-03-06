d3b513c4c2b15aebde3f916e86afcc353dbc9a68
Diane Furlong <dfurlongm@gmail.com>
2021-03-06 12:49:03 -0500

    added schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,35 @@
-### Schema
-DROP DATABASE IF EXISTS burger_db;
-CREATE DATABASE burger_db;
-USE burger_db;
+DROP DATABASE IF EXISTS awty_db;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	devour BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+CREATE DATABASE awty_db;
+
+USE awty_db;
+
+CREATE TABLE trips (
+	id int AUTO_INCREMENT NOT NULL,
+    trip_name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE locations (
+	id int AUTO_INCREMENT NOT NULL,
+    location_name GEOMETRY,
+    trips_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (trips_id) REFERENCES trips(id)
+);
+
+CREATE TABLE users (
+	id int AUTO_INCREMENT NOT NULL,
+	name VARCHAR(255),
+    trips_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (trips_id) REFERENCES trips(id)
+);
+
+CREATE TABLE posts (
+	id int AUTO_INCREMENT NOT NULL,
+    body VARCHAR(255),
+    author VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (author) REFERENCES users(name)
 );
