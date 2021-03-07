27b760d211c125e2e92953a0517ea380cdb772b8
Diane Furlong <dfurlongm@gmail.com>
2021-03-07 12:28:20 -0500

    added db files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO trips (trip_name) VALUES ('weekend roadtrip');
+INSERT INTO locations (city_name, trips_id) VALUES ('Cleveland', 1);
+INSERT INTO people (name, trips_id) VALUES ('Jordan', 1), ('Diane', 1), ('Amber', 1), ('Nick', 1);
