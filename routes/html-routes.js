7659fc0eb9a75ed166a46afd55e41e1721710e87
Jordan Rantucci <jordanrantucci@gmail.com>
2021-03-13 12:23:19 -0500

    adding lodash to html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,37 +11,41 @@ module.exports = function(app) {
 
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-
+    // console.log("html-apiRoutes.js line 14")
     if (req.user) {
-      res.redirect("/mytrips");
+      // res.redirect("/members");
     }
-
+    // res.sendFile(path.join(__dirname, "login"));
     res.render('partials/login')
   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/mytrips");
+      // res.redirect("/members");
 
     }
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
     res.render('partials/login')
   });
 
   // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  // If a user who is not logged in tries to access this route they will be redirected to the signup pageF
+  // Updated mytrips route to include trips_id
   app.get("/mytrips", isAuthenticated, function(req, res) {
     db.trips.findAll({
     }).then(function(result) {
       const trips = _.map(result, "dataValues")
-      const tripObj = {trips: trips}
+      const tripObj = { trips: trips }
       console.log(tripObj)
       // {trips: result.map(data=>{
-        //  return data.dataValues
+      //  return data.dataValues
       // })}
-   
+
 
       res.render('partials/mytrips', tripObj)
+ 
+      
     })
   });
 
@@ -52,12 +56,14 @@ module.exports = function(app) {
 
 
   app.get("/signup", function(req, res) {
+    // console.log("html-routes.js line 43")
     res.render('partials/signup')
   })
 
   app.get("/mytrips/:trip", function(req, res) {
     const trip={id: req.params.trip}
     res.render("partials/trip", trip)
+    // console.log(req.params.trip)
   })
 
   app.delete("/mytrips/:trip", function(req, res) {
