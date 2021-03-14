fb3048e6d9bf0a8da705c0c7fc5ad9a8e49dbd2d
Diane Furlong <dfurlongm@gmail.com>
2021-03-14 14:43:10 -0400

    deleting username references

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,14 +3,11 @@ $(document).ready(function() {
   var signUpForm = $(".signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
-  var nameInput = $("input#name-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
-  // Added name to submit form
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
-      name: nameInput.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
@@ -19,17 +16,15 @@ $(document).ready(function() {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.name, userData.email, userData.password);
-    nameInput.val("");
+    signUpUser(userData.email, userData.password);
     emailInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(name, email, password) {
+  function signUpUser(email, password) {
     $.post("/api/signup", {
-      name: name,
       email: email,
       password: password
     })
